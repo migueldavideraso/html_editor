63ba647bd5402a6ee30ee35f60304aaee674b523
Miguel Eraso <migueldavideraso25@gmail.com>
2023-09-04 09:38:28 -0500

    Fix bug: Error at get value type

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,15 @@
 <script lang="ts">
 
 	import App from './app/App.svelte'
-	import ProofCard from './proof_card/Main.svelte'
-	import { allElementsStore } from '@/file_state/sections'
+	// import ProofCard from './proof_card/Main.svelte'
+	import { allElementsStore } from './file_state/sections'
 	import { cardsSections, elementsProof, simpleProof } from './data'
 
+  if (!allElementsStore.get()) {
+    allElementsStore.set(cardsSections.slice(0, 1))
+  }
 
-	allElementsStore.set(cardsSections.slice(0, 1))
+
 
 </script>
 
