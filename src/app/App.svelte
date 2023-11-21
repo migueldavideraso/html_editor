9f74bfa962886b0eaca3b4b0a15817f3243841e3
Miguel Eraso <96079469+migueldavideraso@users.noreply.github.com>
2023-11-21 17:34:09 -0500

    fix: Separate package and development version styles

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -6,7 +6,7 @@
   let wrapperElement = null
 </script>
 
-<div class="main_container">
+<div class="html-editor-container">
   <main class="container_app" id="html_editor_app" bind:this={wrapperElement}>
     <Result />
   </main>
@@ -21,7 +21,7 @@
 </div>
 
 <style>
-  .main_container {
+  .html-editor-container {
     overflow: hidden;
     display: grid;
     height: 100%;
@@ -30,12 +30,12 @@
     border: 1px solid rgb(172, 173, 188);
   }
 
-  .main_container > main,
-  .main_container > aside {
+  .html-editor-container > main,
+  .html-editor-container > aside {
     height: 100%;
   }
 
-  .main_container > aside {
+  .html-editor-container > aside {
     border-left-style: solid;
     flex-direction: column;
     overflow-y: auto;
@@ -62,94 +62,4 @@
     color: rgba(0, 0, 0, 0.8);
   }
 
-  .main_container :global(.app--button) {
-    justify-content: flex-start;
-    background: transparent;
-    align-items: center;
-    display: flex;
-    grid-gap: 5px;
-    border-radius: 5px;
-    padding: 10px 12.5px;
-    color: rgba(0, 0, 0, 0.8);
-    border: 1px solid rgba(0, 0, 0, 0.15);
-  }
-
-  .main_container :global(.app--button:hover) {
-    opacity: 0.8;
-  }
-
-  .main_container :global(.app--options_section) {
-    position: relative;
-    padding: 10px;
-    min-height: 30px;
-    margin: 0px 20px;
-    border-radius: 0px 0px 5px 5px;
-    background: rgba(0, 0, 0, 0.02);
-    border: 1px solid rgba(0, 0, 0, 0.15);
-    border-top: none;
-  }
-
-  .main_container :global(.app--main_options_section) {
-    padding: 10px;
-    margin: 0px 10px;
-    border-radius: 5px;
-    background: rgba(0, 0, 0, 0.02);
-    border: 1px solid rgba(0, 0, 0, 0.15);
-  }
-
-  .main_container :global(.app--main_options_section .main_title),
-  .main_container :global(.app--options_section .main_title) {
-    font-size: 0.8rem;
-    font-weight: 600;
-    margin: 0px -5px -10px;
-  }
-
-  .main_container :global(.app--main_options_section .title),
-  .main_container :global(.app--options_section .title) {
-    font-size: 0.78rem;
-    margin-bottom: -10px;
-  }
-
-  .main_container :global(.app--main_options_section .input_group),
-  .main_container :global(.app--options_section .input_group) {
-    align-items: center;
-    flex-wrap: wrap;
-    display: flex;
-    grid-gap: 5px;
-  }
-
-  .main_container :global(.app--main_options_section textarea),
-  .main_container :global(.app--main_options_section select),
-  .main_container :global(.app--main_options_section input),
-  .main_container :global(.app--options_section textarea),
-  .main_container :global(.app--options_section select),
-  .main_container :global(.app--options_section input) {
-    border: none;
-    padding: 2px;
-    font-size: 0.8rem;
-    border-radius: 3px;
-    color: rgba(0, 0, 0, 0.75);
-    box-shadow: 1px 1px 3px rgb(0 0 0 / 20%);
-  }
-
-  .main_container :global(.app--main_options_section textarea),
-  .main_container :global(.app--options_section textarea) {
-    font-family: inherit;
-    resize: vertical;
-    width: 100%;
-    height: 25px;
-    padding: 5px;
-    max-width: 100%;
-    min-height: 25px;
-  }
-
-  .main_container :global(.app--main_options_section input),
-  .main_container :global(.app--options_section input) {
-    max-width: 55px;
-  }
-
-  .main_container :global(.app--main_options_section input[type='number']),
-  .main_container :global(.app--options_section input[type='number']) {
-    text-align: right;
-  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 
+import './main.css'
 import { createHTMLEditor } from "./app";
 
 
